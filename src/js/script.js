64e8dc727e3ad9d49ececb02e56a0c5dcc0f6994
Beka Project <fordocs.tj@gmail.com>
2020-04-21 22:53:27 +0500

    MODAL windows completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 var slider = tns({
     container: '.main-slider',
     items: 1,
@@ -8,20 +9,65 @@ var slider = tns({
     nav: true,
     controls: false
   });
-
-  // MODAL
+//MODAL BLOCK
+  //Consultaion block
   $('[data-modal=consultation]').on('click', () => {
     $('.overlay, #consultation').fadeIn('slow');
   });
-  $('.modal__close').on('click', () => {
-    $('.overlay, #consultation, #thanks, #coupon, #comments').fadeOut('slow');
+  $('[data-modal=thanks]').on('click', () => {
+    $('#consultation').hide();
+    $('.overlay, #thanks').fadeIn('slow');
   });
-  $('[data-modal=coupon]').on('click', () => {
-    $('.overlay, #coupon').fadeIn('slow');
+  //Registration block
+  $('[data-modal=registration]').on('click', () => {
+    $('.overlay, #registration').fadeIn('slow');
   });
+  $('[data-modal=code]').on('click', () => {
+    $('#registration').hide();
+    $('.overlay, #code').fadeIn('slow');
+  });
+  //Signin block
+  $('[data-modal=signin]').on('click', () => {
+    $('.overlay, #signin').fadeIn('slow');
+  });
+  //Coupon block
+  // $('[data-modal=coupon]').on('click', () => {
+  //   $('.overlay, #coupon').fadeIn('slow');
+  // });
+  $('[data-modal=coupon]').each(function(i) {
+      $(this).on('click', function() {
+        $('#coupon .title_coupon').text($('.title_products').eq(i).text());
+        $('#coupon .coupon__price').text($('.products__price').eq(i).text());
+        $('#coupon .prices__sale').text($('.products__content-save').eq(i).text());
+        $('.overlay, #coupon').fadeIn('slow');
+      });
+  });
+  //COUPON LINKS BLOCK
+  //Comments
   $('[data-modal=comments]').on('click', () => {
     $('.overlay, #comments').fadeIn('slow');
   });
-  $('.modal__close-comment').on('click', () => {
-    $('.comments, #comments').fadeOut('slow');
+  //Company
+  $('[data-modal=company]').on('click', () => {
+    $('.overlay, #company').fadeIn('slow');
   });
+  //Privacy
+  $('[data-modal=privacy]').on('click', () => {
+    $('.overlay, #privacy').fadeIn('slow');
+  });
+  //Privacy
+  $('[data-modal=addressLinks]').on('click', () => {
+    $('.overlay, #addressLinks').fadeIn('slow');
+  });
+  //Close btn block
+  $('.modal__close').on('click', () => {
+    $('.overlay, #consultation, #registration, #thanks, #code, #signin, #coupon,#comments, #company, #privacy, #addressLinks').fadeOut('slow');
+  }); 
+  $('.modal__close-links').on('click', () => {
+    $('#comments, #company, #privacy, #addressLinks').fadeOut('slow');
+  });  
+
+  
+
+
+   
